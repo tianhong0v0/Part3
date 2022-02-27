0854d32cf80077cdafc5babf0cdef36c7bcd766e
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-02-27 14:37:20 +0800

    use cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 app.use(express.json())
 morgan.token('post', function (req, res) {
   if (req.method === 'POST') {

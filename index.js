2d2ed3f1d6e45ccf06743acf00d20a2c046542cb
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-02-28 07:46:42 +0800

    add build dir, serve static file in express

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('post', function (req, res) {
   if (req.method === 'POST') {
     return JSON.stringify(req.body)

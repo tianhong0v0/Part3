4bc5a961b8e1918eb470a904c144f18c4ef5ead1
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-03-02 20:10:31 +0800

    add getAll(): list all people in db

--- mongo.js
+++ mongo.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-//if there were less than three process.arguments
 const password = process.argv[2]
 const url = `mongodb+srv://fs-part3c-onclass:${password}@cluster0.ux1bg.mongodb.net/phonebook?retryWrites=true&w=majority`
 mongoose.connect(url)
@@ -8,15 +7,26 @@ const personSchema = new mongoose.Schema({
   name: String,
   number: Number,
 })
-
 const Person = mongoose.model('Person', personSchema)
+
+const getAll = () => {
+  Person.find({}).then((result) => {
+    console.log('in getAll')
+    result.forEach((item) => console.log(item))
+    mongoose.connection.close()
+  })
+}
 
 const firstPerson = new Person({
   name: 'wuhu',
   number: 110,
 })
 
-firstPerson.save().then((result) => {
-  console.log('first person saved')
-  mongoose.connection.close()
-})
+// firstPerson.save().then((result) => {
+//   console.log('first person saved')
+//   mongoose.connection.close()
+// })
+
+if (process.argv.length === 3) {
+  getAll()
+}

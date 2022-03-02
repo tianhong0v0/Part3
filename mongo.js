e808b1bee0dadfe02291e55f0e316335a7b0291e
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-03-02 20:21:40 +0800

    exercise 3.12

--- mongo.js
+++ mongo.js
@@ -11,22 +11,30 @@ const Person = mongoose.model('Person', personSchema)
 
 const getAll = () => {
   Person.find({}).then((result) => {
-    console.log('in getAll')
-    result.forEach((item) => console.log(item))
+    console.log('phonebook:')
+    result.forEach((item) => console.log(`${item.name} ${item.number}`))
     mongoose.connection.close()
   })
 }
 
-const firstPerson = new Person({
-  name: 'wuhu',
-  number: 110,
-})
-
-// firstPerson.save().then((result) => {
-//   console.log('first person saved')
-//   mongoose.connection.close()
-// })
+const add = () => {
+  const newName = process.argv[3]
+  const newNumber = process.argv[4]
+  const newPerson = new Person({
+    name: newName,
+    number: newNumber,
+  })
+  newPerson.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
 
 if (process.argv.length === 3) {
   getAll()
+} else if (process.argv.length === 4) {
+  console.log(`Please provide a number for ${process.argv[3]}`)
+  process.exit(1)
+} else if (process.argv.length === 5) {
+  add()
 }

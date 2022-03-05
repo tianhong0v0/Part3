faabe94ac79253193f59824528c29a45f8735ed5
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-03-05 10:21:15 +0800

    exercise 3.17: update an existed person's number

--- index.js
+++ index.js
@@ -99,6 +99,21 @@ app.post('/api/persons', (request, response) => {
   newPerson.save().then((result) => response.json(result))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((result) => {
+      response.json(result)
+    })
+    .catch((error) => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }

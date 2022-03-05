c26162dbe94a369c4ac56583837d87350148d4f7
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-03-05 10:06:25 +0800

    exercise 3.15: delete a person

--- index.js
+++ index.js
@@ -62,10 +62,12 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((item) => item.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 /* app.post('/api/persons', (request, response) => {
